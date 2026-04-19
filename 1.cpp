6e73b1378d5e8a85814ec7d2f4d99bbb35cf1ed7
hariom38 <rajpoothariom900@gmail.com>
2026-04-19 15:53:35 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,1 +1,31 @@
+//count no of nodes in the linked list
+#include<iostream>
+using namespace std;
+struct Node{
+    public:
+    int data;
+    Node*next;
+};
+int count_nodes(Node*head){
+    int count=0;
+    Node*curr=head;
+    while(curr !=NULL){
+        count++;
+        curr=curr->next;
+    }
+    return count;
+}
+int main(){
+    cout<<"Radhe Radhe"<<endl;
+    Node*head=new Node();
+    head->data=1;
+    head->next=new Node();
+    head->next->data=2;
+    head->next->next=new Node();
+    head->next->next->data=3;
+    head->next->next->next=NULL;
+    cout<<"Number of Nodes->"<<count_nodes(head);
 
+    return 0;
+}
+// output------>Number of Nodes->3
